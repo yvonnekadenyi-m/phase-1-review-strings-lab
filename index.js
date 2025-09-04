70092c99c43b3492905e3bd157ade1edae612432
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-09-04 19:15:59 +0300

    Added strings lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+//const welcomeMessage = "Welcome to Flatbook, Grace Hopper";
+const welcomeMessage = 'Welcome to Flatbook, ${Grace Hopper}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+//const shortGreeting = 'Welcome, ';
+const shortGreeting = 'Welcome, ${G}!';
